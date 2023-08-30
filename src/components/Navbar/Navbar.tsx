0c12d9808f36165cc1c20961cc9e5960e35cce25
Ardee De Ocera <102879568+ArdeeJ@users.noreply.github.com>
2023-08-30 16:27:48 -0600

    Navbar component created need to add functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,47 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+  position: fixed;
+  top: 1in;
   width: 100vw;
   height: 130px;
 
-  
   display: flex;
   background-color: #1a1a1a;
+  z-index: 1000;
+`;
+
+const Logo = styled.div`
+  font-size: 95px;
+  color: #9e3131;
+  font-family: "Black Han Sans", sans-serif;
+  margin: auto;
+  text-shadow: 0px 15px 10px #000000;
+  -webkit-text-stroke: 3px black;
+`;
+const About = styled.div`
+  font-size: 40px;
+  color: #9e3131;
+  font-family: "Black Han Sans", sans-serif;
+  margin: auto;
+  text-shadow: 0px 10px 10px #000000;
+  -webkit-text-stroke: 2px black;
+`;
+const Contact = styled.div`
+  font-size: 40px;
+  color: #9e3131;
+  font-family: "Black Han Sans", sans-serif;
+  margin: auto;
+  text-shadow: 0px 10px 10px #000000;
+  -webkit-text-stroke: 2px black;
 `;
 
 function Navbar() {
   return (
     <Container>
-      <h1>Hello</h1>
+      <About>ABOUT</About>
+      <Logo>AUTEAMATE</Logo>
+      <Contact>CONTACT</Contact>
     </Container>
   );
 }
