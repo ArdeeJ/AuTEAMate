87d97153c401139527f639d5c33d4c5a67c3ca67
Ardee De Ocera <102879568+ArdeeJ@users.noreply.github.com>
2023-08-30 19:06:36 -0600

    Added functional routing to links in navbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,26 @@
-import Navbar from '../components/Navbar/Navbar';
+import styled from "styled-components";
+import Navbar from "../components/Navbar/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const VL = styled.div`
+  border: 3px solid #000000;
+  height: 1200px;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 58%;
+`;
 
 function Home() {
   return (
-    <Navbar />
+    <Container>
+      <VL />
+      <Navbar />
+    </Container>
   );
 }
 
-export default Home
+export default Home;
